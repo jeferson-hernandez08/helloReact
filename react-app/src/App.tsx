e8b9d1e33734d1ee4ff25ba76ce4240d9898a8a0
Jeferson <jefer.hernandez1@gmail.com>
2025-11-28 17:34:31 -0500

    first commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return <p>Hola Mundo</p>;
+
+}
+
+export default App;
+
+
+
+
+/*
+  Componentes
+  - Todas las aplicaciones en react necesitan una funcion que va llamar App, este nombre podria ser otro,
+  asi que necesitamos exportar esta funcion.
+  - Retornamos el contenido que react le va mostrar a los usuarios y aquí 
+
+*/
