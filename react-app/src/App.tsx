e5e686b65becb5bf6c9fd26b6b596004fa1f3642
Jeferson <jefer.hernandez1@gmail.com>
2025-11-29 12:46:25 -0500

    Study Componentes React

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,6 @@
 function App() {
-  return <p>Hola Mundo</p>;
+  const nombre = "Jeferson Hernandez"
+  return <p>Hola {nombre}</p>;
 
 }
 
@@ -12,10 +13,14 @@ export default App;
   Componentes
   - Todas las aplicaciones en react necesitan una funcion que va llamar App, este nombre podria ser otro,
   asi que necesitamos exportar esta funcion.
-  - Retornamos el contenido que react le va mostrar a los usuarios y aquí 
+  - Retornamos el contenido que react le va mostrar a los usuarios y aquí, este contenido podemos ver que 
+    hay codigo HTML dentro de Codigo javaScript, esto es lo que llamamoos codigo JSX, el codigo JSX es muy 
+    parecido a codigo JSX. 
+    Este codigo JSX es transformado a otro codigo que JavaScript si va poder leer, esto se transforma a 
+    JSX -> React.createElement y este va tener unas cuantas opciones:  
 
 */
 
 /*
-  Min: 22:42
+  Min: 25:26
 */
