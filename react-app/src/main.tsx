ab791b16eca2a8f14dab63659db2ceeecce2f1be
Jeferson <jefer.hernandez1@gmail.com>
2025-12-09 20:49:59 -0500

    Start Component

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+//import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+
+/*
+  Min: 37:30 --- -- Study -- 
+*/
