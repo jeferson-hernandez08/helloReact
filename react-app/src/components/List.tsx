59052df1d318b22a6d4e9cf7d880d2cff89259d3
Jeferson <jefer.hernandez1@gmail.com>
2025-12-16 21:48:11 -0500

    Events theme

--- react-app/src/components/List.tsx
+++ react-app/src/components/List.tsx
@@ -6,7 +6,7 @@ function List({ data }: Props) {
   return (
     <ul className="list-group">
        {data.map((elemento) => (
-        <li key={elemento} className="list-group-item">{elemento}</li>
+        <li onClick={() => console.log(elemento)} key={elemento} className="list-group-item">{elemento}</li>
        ))}
     </ul>
   );
