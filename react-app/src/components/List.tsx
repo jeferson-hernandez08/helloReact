b33ecbb73fc9132369f2586cb4e0039b99f96112
Jeferson <jefer.hernandez1@gmail.com>
2025-12-18 13:34:51 -0500

    Events theme finished

--- react-app/src/components/List.tsx
+++ react-app/src/components/List.tsx
@@ -1,12 +1,17 @@
+import { MouseEvent } from "react";
+
 type Props = {
   data: string[];
 }
 
 function List({ data }: Props) {
+    const handleClick = (e: string) => {
+      console.log(e) 
+    }
   return (
     <ul className="list-group">
        {data.map((elemento) => (
-        <li onClick={() => console.log(elemento)} key={elemento} className="list-group-item">{elemento}</li>
+        <li onClick={() => handleClick(elemento)} key={elemento} className="list-group-item">{elemento}</li>
        ))}
     </ul>
   );
